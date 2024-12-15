bc7d1806df7abf68fe93e1728f56838f93536a58
Sam De Wispelaere <samdewispelaere@gmail.com>
2024-12-15 11:06:04 +0100

    code update

--- Code/TotalCode/TotalCode.ino
+++ Code/TotalCode/TotalCode.ino
@@ -5,22 +5,22 @@
 #include <WiFiUdp.h> // Voor UDP communicatie
 
 // Stepper instellen
-#define STEP_PIN 4 // GPIO4 voor PUL+
-#define DIR_PIN 5  // GPIO5 voor DIR+
+#define STEP_PIN 4  // GPIO4 voor PUL+
+#define DIR_PIN 5    // GPIO5 voor DIR+
 
 // Wi-Fi-instellingen
-const char *ssid = "devbit";         // Wi-Fi SSID
-const char *password = "Dr@@dloos!"; // Wi-Fi wachtwoord
+const char* ssid = "devbit";       // Wi-Fi SSID
+const char* password = "Dr@@dloos!";   // Wi-Fi wachtwoord
 
-WebServer server(80); // Initialiseer webserver op poort 80
-WiFiUDP udp;          // Voor communicatie met de smartplug
+WebServer server(80);  // Initialiseer webserver op poort 80
+WiFiUDP udp;           // Voor communicatie met de smartplug
 
 // WiZ smartplug-instellingen
-const char *deviceIP = "10.10.2.77"; // Vervang met het IP-adres van je WiZ smartplug
+const char* deviceIP = "10.10.2.77"; // Vervang met het IP-adres van je WiZ smartplug
 const int udpPort = 38899;
 
 // Variabelen om ontvangen gegevens op te slaan
-long long temperature = -1;       // Begin met een ongeldige waarde
+long long temperature = -1; // Begin met een ongeldige waarde
 long long brightnessSensor = 128; // Standaard helderheid
 int brightnessLEDS = 0;
 
@@ -29,14 +29,129 @@ int brightnessLEDS = 0;
 #define DATA_PIN 2
 CRGB leds[NUM_LEDS];
 
-#define TIME_FACTOR_HUE 60
-#define TIME_FACTOR_SAT 100
-#define TIME_FACTOR_VAL 100
+// Functie om de WiZ smartplug te bedienen
+void sendWiZCommand(bool state) {
+  // Maak JSON-opdrach
 
-void setup()
-{
+  String command = String("{\"method\":\"setState\",\"params\":{\"state\":") +
+                   (state ? "true" : "false") +
+                   String("}}");
+
+  // Stuur de opdracht naar de WiZ smartplug
+  udp.beginPacket(deviceIP, udpPort);
+  udp.print(command);
+  udp.endPacket();
+
+  Serial.print("WiZ Command sent: ");
+  Serial.println(command);
+}
+
+bool ShadeIsOpen = false;   // als true = schaduw is er over de serre
+
+// Functie om ontvangen JSON-gegevens te verwerken
+void handleSensorData() {
+  StaticJsonDocument<200> doc;
+
+  if (server.hasArg("plain")) {
+    DeserializationError error = deserializeJson(doc, server.arg("plain"));
+    if (error) {
+      server.send(400, "application/json", "{\"error\":\"Invalid JSON\"}");
+      return;
+    }
+
+    // Update temperatuur en helderheid alleen bij succesvolle parsing
+    if (doc.containsKey("temperature")) {
+      temperature = doc["temperature"].as<long long>();
+    }
+    if (doc.containsKey("brightness")) {
+      brightnessSensor = doc["brightness"].as<long long>();
+    }
+
+    // Print de ontvangen waarden naar de seriële monitor
+    Serial.print("Received Temperature: ");
+    Serial.println(temperature);
+    Serial.print("Received Brightness: ");
+    Serial.println(brightnessSensor);
+
+  
+    if (brightnessSensor < 270) {
+      if (brightnessLEDS >= 235)
+      {
+        brightnessLEDS = 255;
+      } else {
+        brightnessLEDS += 10;
+      }
+      FastLED.setBrightness(brightnessLEDS);
+      FastLED.show();
+      Serial.print("LEDs more light +10 they are now:");
+      Serial.println(brightnessLEDS);
+    } 
+    else if (brightnessSensor > 700) {
+      if (brightnessLEDS >= 20)
+      {
+        brightnessLEDS = 0;
+      } else {
+        brightnessLEDS -= 10;
+      }
+      FastLED.setBrightness(brightnessLEDS);
+      FastLED.show();
+      Serial.print("LEDs less light (-10) they are now:");
+      Serial.println(brightnessLEDS);
+    }
+
+
+    // Controleer of de temperatuur onder de 20 graden is
+    if (temperature < 20) {
+      Serial.println("Temperature below 20°C: Turning smart plug ON");
+      sendWiZCommand(true); // Zet de smartplug aan
+    } else if(temperature>25) {
+      Serial.println("Temperature 20°C or above: Turning smart plug OFF");
+      sendWiZCommand(false); // Zet de smartplug uit
+    }
+
+
+    if (temperature>30 && ShadeIsOpen == false) {
+      OpenShade();
+    } else if(temperature<25 && ShadeIsOpen == true){
+      CloseShade();
+    }
+
+    // Stuur een bevestiging terug naar de client
+    server.send(200, "application/json", "{\"status\":\"Data received\"}");
+  } else {
+    server.send(400, "application/json", "{\"error\":\"No body provided\"}");
+  }
+}
+
+void OpenShade(){
+  ShadeIsOpen = true;
+
+  digitalWrite(DIR_PIN, LOW);  // Draairichting vooruit instellen
+  Serial.println("Going to OpenShade temp getting high");
+  for (int i = 0; i < 2400; i++) { // 200 keer lus doorlopen is 360 graden draaien
+    digitalWrite(STEP_PIN, HIGH); // omtrek buizen van ons 12.5 cm doek moet 1.6m uit rollen = ong 12 rotaties = 200*12 = 2400
+    delayMicroseconds(1000);  // Snelheid verhogen, kleiner delay
+    digitalWrite(STEP_PIN, LOW);
+    delayMicroseconds(1000);
+  }
+}
+
+void CloseShade(){
+  ShadeIsOpen = false;
+
+  digitalWrite(DIR_PIN, HIGH);  // Draairichting vooruit instellen
+  Serial.println("Going to CloseShade temp getting low");
+  for (int i = 0; i < 2400; i++) { // 200 keer lus doorlopen is 360 graden draaien
+    digitalWrite(STEP_PIN, HIGH); // omtrek buizen van ons 12.5 cm doek moet 1.6m uit rollen = ong 12 rotaties = 200*12 = 2400
+    delayMicroseconds(1000);  // Snelheid verhogen, kleiner delay
+    digitalWrite(STEP_PIN, LOW);
+    delayMicroseconds(1000);
+  }
+}
+
+void setup() {
   Serial.begin(115200);
-
+  
   // stepper als output zetten
   pinMode(DIR_PIN, OUTPUT);
   pinMode(STEP_PIN, OUTPUT);
@@ -47,8 +162,7 @@ void setup()
   WiFi.begin(ssid, password);
 
   Serial.print("Connecting to Wi-Fi");
-  while (WiFi.status() != WL_CONNECTED)
-  {
+  while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.print(".");
   }
@@ -64,168 +178,22 @@ void setup()
   server.begin();
   Serial.println("HTTP server started");
 
-  // Initialize LEDs met RGB-configuratie
-  FastLED.addLeds<WS2812, DATA_PIN, GRB>(leds, NUM_LEDS);
+  // Initialize LEDs met RGBW-configuratie
+  FastLED.addLeds<WS2812, DATA_PIN, GRB>(leds, NUM_LEDS).setRgbw(RgbwDefault());
+
+  // volledige ledstrip op paars zetten
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = CRGB(255, 255, 0); // Paars
+  }
+  FastLED.show(); // Update de LEDs
 
   // Initializeer UDP voor de WiZ smartplug
   udp.begin(udpPort);
   Serial.println("UDP Initialized");
 }
 
-// Functie om de WiZ smartplug te bedienen
-void sendWiZCommand(bool state)
-{
-  // Maak JSON-opdracht
-  String command = String("{\"method\":\"setState\",\"params\":{\"state\":") +
-                   (state ? "true" : "false") +
-                   String("}}");
-
-  // Stuur de opdracht naar de WiZ smartplug
-  udp.beginPacket(deviceIP, udpPort);
-  udp.print(command);
-  udp.endPacket();
-
-  Serial.print("WiZ Command sent: ");
-  Serial.println(command);
-}
-
-bool ShadeIsOpen = false; // als true = schaduw is er over de serre
-
-// Functie om ontvangen JSON-gegevens te verwerken
-void handleSensorData()
-{
-  StaticJsonDocument<200> doc;
-
-  if (server.hasArg("plain"))
-  {
-    DeserializationError error = deserializeJson(doc, server.arg("plain"));
-    if (error)
-    {
-      server.send(400, "application/json", "{\"error\":\"Invalid JSON\"}");
-      return;
-    }
-
-    // Update temperatuur en helderheid alleen bij succesvolle parsing
-    if (doc.containsKey("temperature"))
-    {
-      temperature = doc["temperature"].as<long long>();
-    }
-    if (doc.containsKey("brightness"))
-    {
-      brightnessSensor = doc["brightness"].as<long long>();
-    }
-
-    // Print de ontvangen waarden naar de seriële monitor
-    Serial.print("Received Temperature: ");
-    Serial.println(temperature);
-    Serial.print("Received Brightness: ");
-    Serial.println(brightnessSensor);
-
-    if (brightnessSensor < 270)
-    {
-      if (brightnessLEDS < 235)
-      {
-        brightnessLEDS += 10;
-      }
-      else
-      {
-        brightnessLEDS = 255;
-      }
-      FastLED.setBrightness(brightnessLEDS);
-      Serial.print("LEDs more light +10 they are now: ");
-      Serial.println(brightnessLEDS);
-    }
-    else if (brightnessSensor > 700)
-    {
-      if (brightnessLEDS > 10)
-      {
-        brightnessLEDS -= 10;
-      }
-      else
-      {
-        brightnessLEDS = 0;
-      }
-      FastLED.setBrightness(brightnessLEDS);
-      Serial.print("LEDs less light (-10) they are now: ");
-      Serial.println(brightnessLEDS);
-    }
-
-    // Controleer of de temperatuur onder de 20 graden is
-    if (temperature < 20)
-    {
-      Serial.println("Temperature below 20°C: Turning smart plug ON");
-      sendWiZCommand(true); // Zet de smartplug aan
-    }
-    else if (temperature > 25 && temperature < 27)
-    {
-      Serial.println("Temperature between 25°C and 27°C: Turning smart plug OFF");
-      sendWiZCommand(false); // Zet de smartplug uit
-    }
-
-    if (temperature > 30 && !ShadeIsOpen)
-    {
-      OpenShade();
-    }
-    else if (temperature < 25 && ShadeIsOpen)
-    {
-      CloseShade();
-    }
-
-    // Stuur een bevestiging terug naar de client
-    server.send(200, "application/json", "{\"status\":\"Data received\"}");
-  }
-  else
-  {
-    server.send(400, "application/json", "{\"error\":\"No body provided\"}");
-  }
-}
-
-void OpenShade()
-{
-  ShadeIsOpen = true;
-
-  digitalWrite(DIR_PIN, LOW); // Draairichting vooruit instellen
-  Serial.println("Going to OpenShade temp getting high");
-  for (int i = 0; i < 200; i++)
-  {
-    digitalWrite(STEP_PIN, HIGH);
-    delay(10);
-    digitalWrite(STEP_PIN, LOW);
-    delay(10);
-  }
-}
-
-void CloseShade()
-{
-  ShadeIsOpen = false;
-
-  digitalWrite(DIR_PIN, HIGH); // Draairichting achteruit instellen
-  Serial.println("Going to CloseShade temp getting low");
-  for (int i = 0; i < 200; i++)
-  {
-    digitalWrite(STEP_PIN, HIGH);
-    delay(10);
-    digitalWrite(STEP_PIN, LOW);
-    delay(10);
-  }
-}
-
-
-
-void loop()
-{
+void loop() {
   // Verwerk binnenkomende HTTP-verzoeken
   server.handleClient();
-
-  // Initialize LEDs with RGBW configuration
-    FastLED.addLeds<WS2812, DATA_PIN, GRB>(leds, NUM_LEDS).setRgbw(RgbwDefault());
-    FastLED.setBrightness(255);  // Set global brightness to 50%
-  // Set all LEDs to purple (mix of red and blue)
-  for (int i = 0; i < NUM_LEDS; i++)
-  {
-    leds[i] = CRGB(255, 255, 0); // Rood , Blauw , Groen
-  }
-  FastLED.show(); // Update the LEDs
-
-  delay(5000); // Iedere 5 sec binnen lezen enz
+  delay(5000); // iedere 5 sec binnen lezen enz
 }
